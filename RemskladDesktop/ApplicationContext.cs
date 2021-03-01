e4965f5146250839ca7a60e99404d764b1407575
Сергей Кулишов <Rewq95_kso@mail.ru>
2021-03-01 17:35:01 +0300

    Trying to solve issue with database

---
+++ RemskladDesktop/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace RemskladDesktop
+
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Datum> Datums { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=remskladapi;Trusted_Connection=True;");
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=greatsteve");
+
+        }
+    }
+}
--- RemskladDesktop/MainWindow.xaml.cs
+++ RemskladDesktop/MainWindow.xaml.cs
@@ -23,11 +23,94 @@ namespace RemskladDesktop
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
-        private void phonesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void Button_Accums(object sender, RoutedEventArgs e)
+        {
+            var art = Repository.articlesOfAccums;
+            List<Datum> data = Repository.FetchData();
+            List<Datum> filtered = new List<Datum>();
+            foreach (var article in art)
+            {
+                foreach (var item in data)
+                {
+                    if (item.article == article)
+                    {
+                        filtered.Add(item);
+                    }
+                }
+            }
+            ItemList.ItemsSource = filtered.Reverse<Datum>();
+
+        }
+
+        private void Button_DispOrig(object sender, RoutedEventArgs e)
+        {
+            var art = Repository.articlesOfDisplayOrig;
+            List<Datum> data = Repository.FetchData();
+            List<Datum> filtered = new List<Datum>();
+            foreach (var article in art)
+            {
+                foreach (var item in data)
+                {
+                    if (item.article == article)
+                    {
+                        filtered.Add(item);
+                    }
+                }
+            }
+            ItemList.ItemsSource = filtered.Reverse<Datum>();
+
+        }
+
+        private void Button_DispCopy(object sender, RoutedEventArgs e)
         {
 
         }
+
+        private async void UpdateDB(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Dictionary<string, Datum> ItemsFromWarehouse = ConnectionWithRemonline.GetItemByArticle(await ConnectionWithRemonline.GetCollectionOfItems(), Repository.GetAllArticlesOfItemWhatWeNeed());
+                Repository.Update(ItemsFromWarehouse);
+                UpdateButton.Background = Brushes.Green;
+                await Task.Delay(10000);
+                UpdateButton.Background = Brushes.White;
+            }
+            catch (Exception ex)
+            {
+                UpdateButton.Background = Brushes.Red;
+                await Task.Delay(10000);
+                UpdateButton.Background = Brushes.White;
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine("Обновление не удалось");
+                Console.WriteLine("Updating is fail");
+            }
+        }
+
+        private async void CreateDB(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Dictionary<string, Datum> ItemsFromWarehouse = ConnectionWithRemonline.GetItemByArticle(await ConnectionWithRemonline.GetCollectionOfItems(), Repository.GetAllArticlesOfItemWhatWeNeed());
+                Repository.Add(ItemsFromWarehouse);
+                CreateButton.Background = Brushes.Green;
+                await Task.Delay(10000);
+                CreateButton.Background = Brushes.White;
+
+            }
+            catch (Exception ex)
+            {
+                CreateButton.Background = Brushes.Red;
+                await Task.Delay(10000);
+                CreateButton.Background = Brushes.White;
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine("Обновление не удалось");
+                Console.WriteLine("Updating is fail");
+            }
+        }
     }
 }
+
