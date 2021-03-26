cafc47b5b2813a6354b784472dba331b09b29a16
Сергей Кулишов <rewq95kso@yandex.ru>
2021-03-26 18:05:31 +0300

    Start working on ReportSystem

---
+++ RemskladDesktop/Reporter/Reporter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemskladDesktop.Reporter
+{
+    class Reporter
+    {
+
+        public static string CreateReport()
+        {
+            var s = Repository.FetchData();
+            string msg = "";
+            foreach (var item in s)
+            {
+                msg += $"<b>{item.title} - Остаток :{item.residue}</b>\n";
+            }
+            return msg;
+        }
+    }
+}
---
+++ RemskladMailer/Mailer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+
+namespace RemskladMailer
+{
+    public class Mailer
+    {
+        public static async Task SendEmailAsync(string message= "")
+        {
+            MailAddress from = new MailAddress("sigurtholegsson@gmail.com", "Уведомление от Remsklad");
+            MailAddress to = new MailAddress("rewq95kso@yandex.ru");
+            MailMessage m = new MailMessage(from, to);
+            m.Subject = "Остатки на складе";
+            m.Body = message;
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.Credentials = new NetworkCredential("sigurtholegsson@gmail.com", "R911e007");
+            smtp.EnableSsl = true;
+            await smtp.SendMailAsync(m);
+            Console.WriteLine("Письмо отправлено");
+        }
+
+        
+    }
+}
