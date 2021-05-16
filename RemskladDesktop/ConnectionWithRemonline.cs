3f3dc71099ec4807c06075721e17bab673f731a7
SergeiKulishov <rewq95kso@yandex.ru>
2021-05-16 19:54:18 +0300

    Work on ConnectionWithRemonline

--- RemskladDesktop/ConnectionWithRemonline.cs
+++ RemskladDesktop/ConnectionWithRemonline.cs
@@ -14,17 +14,12 @@ namespace RemskladDesktop
         public static async Task PostRequestAsync()
         {
             WebRequest request = WebRequest.Create("https://api.remonline.ru/token/new");
-            request.Method = "POST"; // для отправки используется метод Post
-                                     // данные для отправки
+            request.Method = "POST"; 
             string data = "api_key=a49d338c1466430e9568ca6ea77c5cda";
-            // преобразуем данные в массив байтов
             byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(data);
-            // устанавливаем тип содержимого - параметр ContentType
             request.ContentType = "application/x-www-form-urlencoded";
-            // Устанавливаем заголовок Content-Length запроса - свойство ContentLength
             request.ContentLength = byteArray.Length;
 
-            //записываем данные в поток запроса
             using (Stream dataStream = request.GetRequestStream())
             {
                 dataStream.Write(byteArray, 0, byteArray.Length);
@@ -35,7 +30,7 @@ namespace RemskladDesktop
             {
                 using (StreamReader reader = new StreamReader(stream))
                 {
-                    string token = reader.ReadToEnd();
+                    string token = await reader.ReadToEndAsync();
                     ConnectionWithRemonline.responseToken = token;
                 }
             }
@@ -53,27 +48,21 @@ namespace RemskladDesktop
 
         }
 
-        public static string GetPageFromRemonline(string token,int page)
+        public static async Task<string> GetPageFromRemonline(string token,int page)
         {
-            // Адрес ресурса, к которому выполняется запрос
             string url = $"https://api.remonline.ru/warehouse/goods/28208?page={page}&token={token}";
-
-            // Создаём объект WebClient
             using  (var webClient = new WebClient())
             {
-                // Выполняем запрос по адресу и получаем ответ в виде строки
-                var response =  webClient.DownloadString(url);
-                
+                string response = await webClient.DownloadStringTaskAsync(url);
                 return response;
             }
-            
         }
 
         public static async Task<List<Item>> GetCollectionOfItems(int pageCount = 29){
             List<Item> ListItem = new List<Item>();
             for (int i = 1; i <= pageCount; i++)
             {
-                string responceMessage = ConnectionWithRemonline.GetPageFromRemonline(await ConnectionWithRemonline.GetToken(), i);
+                string responceMessage = await ConnectionWithRemonline.GetPageFromRemonline(await ConnectionWithRemonline.GetToken(), i);
                 Item thing = JsonConvert.DeserializeObject<Item>(responceMessage);
                 ListItem.Add(thing);
             }
